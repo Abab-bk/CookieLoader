5cce3d63ca85e663ea61d4fe09fde2bb663a1937
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2025-01-25 21:07:15 +0800

    refactor: Add BaseAssetsLoader

---
+++ CookieLoader/BaseAssetLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using Godot;
+
+namespace CookieLoader;
+
+public abstract class BaseAssetLoader(float minLoadDuration)
+{
+    public event Action OnLoadComplete = delegate { };
+    
+    protected float ElapsedTime { get; set; }
+    protected float MinLoadDuration => minLoadDuration;
+    protected bool CompleteEventFired { get; set; }
+
+    public void Process(float delta)
+    {
+        ElapsedTime += delta;
+        ProcessStep(delta);
+        CheckEvent();
+    }
+
+    protected abstract void ProcessStep(float delta);
+    public abstract Error Start();
+    public abstract bool IsComplete();
+
+    public string GetLoadDuration() => ElapsedTime.ToString(CultureInfo.CurrentCulture);
+    public float GetLoadDurationFloat() => ElapsedTime;
+
+    protected void CheckEvent()
+    {
+        if (CompleteEventFired || !IsComplete()) return;
+        
+        CompleteEventFired = true;
+        OnLoadComplete();
+    }
+}
--- CookieLoader/MultiAssetLoader.cs
+++ CookieLoader/MultiAssetLoader.cs
@@ -1,53 +1,36 @@
-﻿using System.Globalization;
-using Godot;
+﻿using Godot;
 using Array = Godot.Collections.Array;
 
 namespace CookieLoader;
 
-public class MultiAssetLoader
+public class MultiAssetLoader : BaseAssetLoader
 {
-    public event Action OnLoadComplete = delegate { };
     public event Action<LoadItem> OnSingleAssetLoadComplete = delegate { };
 
-    private readonly Array _progress = new ();
     private readonly LoadItem[] _loadItems;
     private LoadItem? _currentItem;
-    
-    private bool _completeEventFired;
-    
-    private readonly float _minLoadDuration;
-    private float _elapsedTime;
-    
-    public MultiAssetLoader(IEnumerable<string> paths, float minLoadDuration = 0f)
+    private readonly Array _progress = new ();
+
+    public MultiAssetLoader(IEnumerable<string> paths, float minLoadDuration = 0f) 
+        : base(minLoadDuration)
     {
         var pathArray = paths.ToArray();
         _loadItems = new LoadItem[pathArray.Length];
         
         for (var i = 0; i < pathArray.Length; i++)
-        {
             _loadItems[i] = new LoadItem(pathArray[i]);
-        }
-
-        _minLoadDuration = minLoadDuration;
     }
 
     public LoadItem GetLoadItem(int index) => _loadItems[index];
-    public LoadItem? GetLoadItem(string path) =>
+    public LoadItem? GetLoadItem(string path) => 
         _loadItems.FirstOrDefault(item => item.Path == path);
     
-    public T? Get<T>(int index) where T : Resource
-    {
-        if (!_loadItems[index].Finished) return null;
-        return _loadItems[index].Resource as T;
-    }
+    public T? Get<T>(int index) where T : Resource => 
+        _loadItems[index].Finished ? _loadItems[index].Resource as T : null;
     
     public T? Get<T>(string path) where T : Resource =>
-        _loadItems.All(item => item.Path != path) ? null :
-            Get<T>(_loadItems.ToList().FindIndex(item => item.Path == path));
-    
-    public string GetLoadDuration() => _elapsedTime.ToString(CultureInfo.CurrentCulture);
-    public float GetLoadDurationFloat() => _elapsedTime;
-    
+        GetLoadItem(path) is { Finished: true } item ? item.Resource as T : null;
+
     public float GetTotalProgress()
     {
         var completed = _loadItems.Count(item => item.Finished);
@@ -60,7 +43,7 @@ public class MultiAssetLoader
         
         currentProgress = status switch
         {
-            ResourceLoader.ThreadLoadStatus.InProgress when _progress.Count > 0
+            ResourceLoader.ThreadLoadStatus.InProgress when _progress.Count > 0 
                 => (int)_progress[0],
             ResourceLoader.ThreadLoadStatus.Loaded => 1f,
             _ => currentProgress
@@ -68,14 +51,11 @@ public class MultiAssetLoader
 
         return (completed + currentProgress) / _loadItems.Length;
     }
-    
-    public Error Start() => LoadNextAvailable();
-    
-    public void Process(float delta)
+
+    public override Error Start() => LoadNextAvailable();
+
+    protected override void ProcessStep(float delta)
     {
-        _elapsedTime += delta;
-        CheckEvent();
-        
         if (_currentItem == null || _currentItem.Finished) return;
 
         var status = ResourceLoader.LoadThreadedGetStatus(_currentItem.Path, _progress);
@@ -86,45 +66,28 @@ public class MultiAssetLoader
             case ResourceLoader.ThreadLoadStatus.InvalidResource:
                 MarkCurrentFinishedAndLoadNext();
                 break;
-            case ResourceLoader.ThreadLoadStatus.InProgress:
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
         }
     }
-    
+
     private Error LoadNextAvailable()
     {
         var nextItem = _loadItems.FirstOrDefault(item => !item.Finished);
-        if (nextItem == null)
-        {
-            _currentItem = null;
-            return Error.Ok;
-        }
+        if (nextItem == null) return Error.Ok;
         
         _currentItem = nextItem;
         return ResourceLoader.LoadThreadedRequest(nextItem.Path);
     }
 
-    private void CheckEvent()
-    {
-        if (_completeEventFired) return;
-        if (!IsComplete()) return;
-        
-        _completeEventFired = true;
-        OnLoadComplete();
-    }
-
     private void MarkCurrentFinishedAndLoadNext()
     {
-        if (_currentItem == null) return;
-        if (_currentItem.Finished) return;
+        if (_currentItem == null || _currentItem.Finished) return;
+        
         _currentItem.Finished = true;
         _currentItem.Resource = ResourceLoader.LoadThreadedGet(_currentItem.Path);
         OnSingleAssetLoadComplete(_currentItem);
         LoadNextAvailable();
     }
-    
-    public bool IsComplete() => _loadItems.All(item => item.Finished) &&
-                                _elapsedTime >= _minLoadDuration;
+
+    public override bool IsComplete() => 
+        _loadItems.All(item => item.Finished) && ElapsedTime >= MinLoadDuration;
 }
